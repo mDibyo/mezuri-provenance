624f54a7085310e663c18f8c985dabcb7b50a14d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-05-06 23:54:22 -0700

    Add interface generate for generating IOP spec

--- cli/interface.py
+++ cli/interface.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+from os.path import relpath
 
-from .utils import SPEC_FILENAME, component_init, component_commit, component_publish, Version
+from lib.declarations import DECLARATION_ATTR_INPUT_KEY, DECLARATION_ATTR_OUTPUT_KEY, DECLARATION_ATTR_PARAMETER_KEY
+from utilities.constructs import Version
+from .utils import (
+    SPEC_FILENAME, SPEC_KEY, get_project_root_by_specification,
+    component_context, component_init, extract_component_declaration,
+    component_commit, component_publish
+)
 
 INTERFACE_COMMAND_HELP = 'Work with interfaces.'
+
+DEFAULT_DEFINITION_FILE = 'interface.py'
 
 
 def init(_):
     return component_init()
+
+
+def generate(args):
+    filename = args.file if args.file is not None else DEFAULT_DEFINITION_FILE
+    decl = extract_component_declaration(filename, 'Interface')
+    if decl is None:
+        print('Could not evaluate operator definition file {}'.format(filename))
+
+    with component_context() as ctx:
+        ctx[SPEC_KEY]['iop_declaration'] = {
+            'inputs': {k: v.serialize() for k, v in decl[DECLARATION_ATTR_INPUT_KEY].items()},
+        }
+        ctx[SPEC_KEY]['definition'] = relpath(filename, get_project_root_by_specification())
+    return 0
 
 
 def commit(args):
@@ -27,6 +50,15 @@ def add_interface_commands(parser):
                                              help='Initialize an interface.',
                                              description='Initialize an interface.')
     init_parser.set_defaults(command=init)
+
+    # Generate
+    generate_parser = command_parsers.add_parser('generate',
+                                                 help='Generate the Input specifications from the '
+                                                      'interface definition in the specified file.')
+    generate_parser.set_defaults(command=generate)
+    generate_parser.add_argument('-f', '--file',
+                                 help='The interface definition file. If not provided, {} is '
+                                      'assumed.'.format(DEFAULT_DEFINITION_FILE))
 
     # Commit
     commit_parser = command_parsers.add_parser('commit',
