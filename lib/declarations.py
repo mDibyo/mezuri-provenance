9a5866d7fde4d2d153a5e43fbd111512c36994de
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-05-09 22:58:53 -0700

    Rewrite data types to support deserialization

--- lib/declarations.py
+++ lib/declarations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from abc import ABCMeta, abstractmethod
+from collections import namedtuple
 from typing import Dict as _Dict, Callable
 
 
@@ -8,49 +9,109 @@ PARAM_METHOD_DECLARATION_ATTR = '__mezuri_param_method__'
 IO_METHOD_DECLARATION_ATTR = '__mezuri_io_method__'
 
 
-class MezuriType(metaclass=ABCMeta):
+Serialized = namedtuple('Serialized', ['data_type', 'contents'])
+deserializers = {}
+
+
+def get_deserialized(contents):
+    s = Serialized(*contents)
+    return deserializers[s.data_type].deserialize(s.contents)
+
+
+class MezuriSerializableMeta(ABCMeta):
+    def __init__(cls, name, bases, dct):
+        deserializers[dct['data_type']] = cls
+
+        super(MezuriSerializableMeta, cls).__init__(name, bases, dct)
+
+
+class AbstractMezuriSerializable(metaclass=MezuriSerializableMeta):
+    """
+    An abstract class for objects that can be serialized and deserialized
+    for the purpose of writing out and reading in from a specification file.
+    """
     data_type = NotImplemented
 
     @abstractmethod
     def serialize(self):
         return NotImplemented
 
+    @classmethod
+    @abstractmethod
+    def deserialize(cls, contents):
+        return NotImplemented
 
-class MezuriBaseType(MezuriType, metaclass=ABCMeta):
-    def __init__(self, data_type):
-        self.data_type = data_type
+    @abstractmethod
+    def __repr__(self):
+        return NotImplemented
 
-    def serialize(self):
+
+class MezuriBaseType(AbstractMezuriSerializable):
+    data_type = 'ABSTRACT_BASE'
+
+    @classmethod
+    def serialize(cls):
+        return Serialized(cls.data_type, None)
+
+    @classmethod
+    def deserialize(cls, _):
+        return cls()
+
+    def __repr__(self):
         return self.data_type
 
-Int = MezuriBaseType('INT')
-Bool = MezuriBaseType('BOOL')
-Double = MezuriBaseType('DOUBLE')
-String = MezuriBaseType('STRING')
+
+class Int(MezuriBaseType):
+    data_type = 'INT'
 
 
-class List(MezuriType):
+class Bool(MezuriBaseType):
+    data_type = 'BOOL'
+
+
+class Double(MezuriBaseType):
+    data_type = 'DOUBLE'
+
+
+class String(MezuriBaseType):
+    data_type = 'STRING'
+
+
+class List(AbstractMezuriSerializable):
     data_type = 'LIST'
 
-    def __init__(self, element_type: MezuriType):
+    def __init__(self, element_type: AbstractMezuriSerializable):
         self.element_type = element_type
 
     def serialize(self):
-        return self.data_type, self.element_type.serialize()
+        return Serialized(self.data_type, self.element_type.serialize())
+
+    @classmethod
+    def deserialize(cls, contents: Serialized):
+        return cls(get_deserialized(contents))
+
+    def __repr__(self):
+        return '[{}]'.format(repr(self.element_type))
 
 
-class Dict(MezuriType):
+class Dict(AbstractMezuriSerializable):
     data_type = 'DICT'
 
-    def __init__(self, definition: _Dict[str, MezuriType]):
+    def __init__(self, definition: _Dict[str, AbstractMezuriSerializable]):
         self.definition = definition
 
     def serialize(self):
-        return self.data_type, {k: v.serialize() for k, v in self.definition.items()}
+        return Serialized(self.data_type,
+                          {k: v.serialize() for k, v in self.definition.items()})
 
+    @classmethod
+    def deserialize(cls, contents: _Dict[str, Serialized]):
+        return cls({k: get_deserialized(c) for k, c in contents.items()})
 
-class InterfaceProxy:
-    data_type = 'INTERFACE'
+    def __repr__(self):
+        return '{{{}}}'.format(', '.join('{}: {}'.format(k, repr(v))
+                                         for k, v in self.definition.items()))
+
 
     def __init__(self, interface_registry: str, interface_name: str, version_str: str):
         self.interface_registry = interface_registry
